4fb94cb4701fd7f93ee165af1bfaa61d83065230
sara-siddiquie <100688758+sara-siddiquie@users.noreply.github.com>
2022-05-07 14:43:10 +0530

    created programs
    wrote C++ programs for arithmetic operators.

---
+++ program_1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    float b;
+    double c;
+    char d;
+
+    cout<<sizeof(a)<<endl;
+    cout<<sizeof(b)<<endl;
+    cout<<sizeof(c)<<endl;
+    cout<<sizeof(d);
+
+
+    return 0;
+}
---
+++ program_2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    float a,b,product;
+    product = a*b;
+    cout<<"Enter two numbers"<<endl;
+    cin>>a;
+    cin>>b;
+    product = a*b;
+    cout<<product;
+
+
+
+    return 0;
+}
